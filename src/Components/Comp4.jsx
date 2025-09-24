b46545e2696fab4081d8bdb04289459a5c880e01
HARSHANA D.M.S <121012829+Sandun-COM-80@users.noreply.github.com>
2025-09-24 18:47:36 +0530

    update

--- src/Components/Comp4.jsx
+++ src/Components/Comp4.jsx
@@ -6,7 +6,7 @@ function Comp4() {
   return (
     <div className='box4'>
         <h2>Child 3</h2>
-        <p>Welcome {user===""? "Guest" : user}</p>
+        <p>Welcome{user===""? "Guest" : user}</p>
     </div>
   )
 }
